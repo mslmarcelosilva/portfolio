e825be44c916f2fef9dc26f8dd2c401463810195
Marcelo Silva <mslmarcelosilva@gmail.com>
2023-03-27 20:39:04 -0300

    Portfolio 1 e 2

---
+++ Portfolio/js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", { 
+    strings: ["Desenvolvedor Front End", "Analista de Dados"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+    
